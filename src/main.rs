1c43747c0c3f0aad28242541dde011c7d0cde30c
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-11-11 02:04:26 +0900

    Add Operand enum

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use std::io::{self, Read};
 
+enum Operand {
+    Next,
+    Prev,
+    Increment,
+    Decrement,
+    Print,
+    Read,
+    LoopBegin,
+    LoopEnd,
+}
+
 struct Machine {
     pointer: usize,
     memory: Vec<u8>,
-    commands: Vec<char>,
-    command_index: usize,
+    operands: Vec<Operand>,
+    operand_index: usize,
 }
 
 impl Machine {
     const INITIAL_SIZE: usize = 300000;
 
-    fn new(commands: String) -> Self {
+    fn new(operands: Vec<Operand>) -> Self {
         Machine {
             pointer: 0,
             memory: vec![0; Machine::INITIAL_SIZE],
-            commands: commands.chars().collect(),
-            command_index: 0,
+            operands: operands,
+            operand_index: 0,
         }
     }
 
@@ -23,62 +34,78 @@ impl Machine {
         self.memory[self.pointer]
     }
 
-    fn get_command(&self) -> char {
-        self.commands[self.command_index]
+    fn get_operand(&self) -> &Operand {
+        &self.operands[self.operand_index]
     }
 
     fn is_finished(&self) -> bool {
-        self.command_index < self.commands.len()
+        self.operand_index < self.operands.len()
     }
 
     fn run(&mut self) {
         while self.is_finished() {
-            match self.get_command() {
-                '>' => self.pointer += 1,
-                '<' => self.pointer -= 1,
-                '+' => self.memory[self.pointer] += 1,
-                '-' => self.memory[self.pointer] -= 1,
-                '.' => {
-                    print!("{}", char::from(self.get_value()));
-                },
-                ',' => {
+            match self.get_operand() {
+                &Operand::Next => self.pointer += 1,
+                &Operand::Prev => self.pointer -= 1,
+                &Operand::Increment => self.memory[self.pointer] += 1,
+                &Operand::Decrement => self.memory[self.pointer] -= 1,
+                &Operand::Print => print!("{}", char::from(self.get_value())),
+                &Operand::Read => {
                     let stdin = io::stdin();
                     let mut handle = stdin.lock();
                     handle.read(&mut self.memory[self.pointer..self.pointer+1]).unwrap();
                 },
-                '[' => {
+                &Operand::LoopBegin => {
                     if self.get_value() == 0 {
-                        while self.get_command() != ']' {
-                            self.command_index += 1;
+                        loop {
+                            match self.get_operand() {
+                                &Operand::LoopEnd => break,
+                                _ => self.operand_index += 1,
+                            }
                         }
                     }
                 },
-                ']' => {
+                &Operand::LoopEnd => {
                     if self.get_value() != 0 {
                         let mut num_inner_loops = 1;
                         while num_inner_loops != 0 {
-                            self.command_index -= 1;
-                            match self.get_command() {
-                                '[' => {
-                                    num_inner_loops -= 1;
-                                },
-                                ']' => {
-                                    num_inner_loops += 1;
-                                },
+                            self.operand_index -= 1;
+                            match self.get_operand() {
+                                &Operand::LoopBegin => num_inner_loops -= 1,
+                                &Operand::LoopEnd => num_inner_loops += 1,
                                 _ => {}
                             }
                         }
                     }
                 },
-                _ => {}
             }
-            self.command_index += 1;
+            self.operand_index += 1;
         }
     }
 }
 
+fn char2operand(c: char) -> Option<Operand> {
+    match c {
+        '>' => Some(Operand::Next),
+        '<' => Some(Operand::Prev),
+        '+' => Some(Operand::Increment),
+        '-' => Some(Operand::Decrement),
+        '.' => Some(Operand::Print),
+        ',' => Some(Operand::Read),
+        '[' => Some(Operand::LoopBegin),
+        ']' => Some(Operand::LoopEnd),
+        _ => None
+    }
+}
+
+fn parse(commands: &String) -> Vec<Operand> {
+    commands.chars()
+            .filter_map(|c| char2operand(c))
+            .collect()
+}
+
 fn main() {
     let commands = String::from("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-    let mut machine = Machine::new(commands);
+    let mut machine = Machine::new(parse(&commands));
     machine.run();
 }
